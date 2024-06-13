43cce6147363db94c1c85c7615bf0274a2bf33cd
rafael <rafael.louzanoo@gmail.com>
2024-06-12 21:39:47 -0300

    Subindo controler advice para tratamento de exception

---
+++ app/src/main/java/br/com/gestao/pessoa/exception/Response403Exception.java
@@ -0,0 +1,20 @@
+package br.com.gestao.pessoa.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.RestClientResponseException;
+
+import java.nio.charset.Charset;
+
+public class Response403Exception extends RestClientResponseException {
+
+    private static final long serialVersionUID = 1L;
+
+    public Response403Exception(final String msg) {
+        super(msg, HttpStatus.NOT_FOUND.value(), msg, null, null, null);
+    }
+
+    public Response403Exception(final String message, final int statusCode, final String statusText, final HttpHeaders headers, final byte[] responseBody, final Charset responseCharset) {
+        super(message, statusCode, statusText, headers, responseBody, responseCharset);
+    }
+}
